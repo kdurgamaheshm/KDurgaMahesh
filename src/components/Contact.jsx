f655e004b3b39e8c0812da000d06f78cfbb5cf59
KAMMAMPALLI DURGA MAHESH <kdurgamaheshm@gmail.com>
2025-07-24 22:22:03 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,10 +32,10 @@ const Contact = forwardRef(({ inputRef, formVisible, setFormVisible }, ref) => {
         setIsLoading(true);
         try {
             await emailjs.send(
-  import.meta.env.VITE_EMAILJS_SERVICE_ID,
-  import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-  form,
-  import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+    process.env.REACT_APP_EMAILJS_SERVICE_ID,
+    process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+    form,
+    process.env.REACT_APP_EMAILJS_PUBLIC_KEY
 );
             setSubmitted(true);
             setForm({ name: "", email: "", message: "" });
